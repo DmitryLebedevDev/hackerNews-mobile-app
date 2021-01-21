cd0e36e70aa0632fc887b10645f014ee3fcd1550
Gercog52 <54477063+Gercog52@users.noreply.github.com>
2021-01-21 22:49:14 +0800

    add stories api

--- app/src/main/java/com/example/hackernews/StoriesApi.java
+++ app/src/main/java/com/example/hackernews/StoriesApi.java
@@ -1,14 +1,26 @@
 package com.example.hackernews;
 
+import android.util.Log;
+
 import com.google.gson.Gson;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Function;
+import io.reactivex.rxjava3.internal.operators.observable.ObservableCreate;
+import io.reactivex.rxjava3.observers.DefaultObserver;
+import io.reactivex.rxjava3.observers.DisposableObserver;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
@@ -29,12 +41,20 @@ public class StoriesApi {
     }
 
     private Observable<List<Integer>> getIds() {
-        Request idsRequest
-            = new Request.Builder()
-                         .url("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
-                         .build();
-
         return Observable.create(e -> {
+
+            if(!ids.isEmpty()) {
+                e.onNext(ids);
+                e.onComplete();
+
+                return;
+            }
+
+            Request idsRequest
+                    = new Request.Builder()
+                    .url("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
+                    .build();
+
             Call call = httpClient.newCall(idsRequest);
 
             e.setCancellable(call::cancel);
@@ -46,14 +66,123 @@ public class StoriesApi {
                 }
 
                 @Override
-                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                    e.onNext(response.body());
+                public void onResponse(
+                    @NotNull Call call,
+                    @NotNull Response response
+                ) throws IOException {
+                    Integer[] idsList = gson.fromJson(response.body().string(),Integer[].class);
+                    ids = Arrays.asList(idsList);
+                    e.onNext(ids);
+
+                    e.onComplete();
                 }
             });
         });
     }
 
-    public List<Story> getStories() {
+    public Observable<List<Story>> nextSotries() {
+        return Observable.create(e -> {
+            CompositeDisposable disposable = new CompositeDisposable();
 
+            disposable.add(
+                getIds()
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribeWith(new DisposableObserver<List<Integer>>() {
+                        @Override
+                        public void onNext(@NonNull List<Integer> idsList) {
+                            disposable.add(
+                                getStories(idsList).subscribeWith(new DisposableObserver<List<Story>>() {
+                                    @Override
+                                    public void onNext(@NonNull List<Story> stories) {
+                                        ++currentStep;
+
+                                        e.onNext(stories);
+                                        e.onComplete();
+                                    }
+
+                                    @Override
+                                    public void onError(@NonNull Throwable err) {
+                                        e.onError(err);
+                                    }
+
+                                    @Override
+                                    public void onComplete() {
+
+                                    }
+                                })
+                            );
+                        }
+
+                        @Override
+                        public void onError(@NonNull Throwable err) {
+                            e.onError(err);
+                        }
+
+                        @Override
+                        public void onComplete() {
+
+                        }
+                    })
+            );
+
+            e.setCancellable(disposable::dispose);
+        });
+    }
+
+    private Observable<List<Story>> getStories(List<Integer> ids) {
+        Log.v("start", "" + ids.size());
+        return new ObservableCreate<>((event) -> {
+            List<Story> stories = new ArrayList<>();
+            List<Observable<String>> storiesObservables = new ArrayList<>();
+
+            for(int t=(currentStep-1)*sizeStep; t<currentStep*sizeStep && t<ids.size(); t++) {
+                Request storyReq = new Request.Builder()
+                        .url("https://hacker-news.firebaseio.com/v0/item/" + ids.get(t) + ".json?print=pretty")
+                        .build();
+
+                storiesObservables.add(Observable.create(e -> {
+                    Call call = httpClient.newCall(storyReq);
+
+                    e.setCancellable(call::cancel);
+
+                    httpClient.newCall(storyReq).enqueue(new Callback() {
+                        @Override
+                        public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                            Log.v("Error", e.toString());
+                        }
+
+                        @Override
+                        public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                            e.onNext(response.body().string());
+                            e.onComplete();
+                        }
+                    });
+                }).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).map(e -> (String) e));
+            }
+
+            Observable.merge(storiesObservables)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DefaultObserver<String>() {
+                    @Override
+                    public void onNext(@NonNull String s) {
+                        Log.v("", "new");
+                        stories.add(gson.fromJson(s, Story.class));
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        Log.v("Error", e.toString());
+                        event.onError(e);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        event.onNext(stories);
+                        event.onComplete();
+                    }
+                });
+        });
     }
 }
