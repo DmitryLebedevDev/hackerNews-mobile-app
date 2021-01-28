d02c243e103f2062ca9c38911b56f6b9c19d92ef
Gercog52 <54477063+Gercog52@users.noreply.github.com>
2021-01-28 20:43:56 +0800

    add update actions quantity in view

--- app/src/main/java/com/example/hackernews/ActionsActivity.java
+++ app/src/main/java/com/example/hackernews/ActionsActivity.java
@@ -2,13 +2,80 @@ package com.example.hackernews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.observers.DisposableObserver;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import okhttp3.OkHttpClient;
 
 public class ActionsActivity extends AppCompatActivity {
+    ActionsApi  actionsInfo = new ActionsApi(new OkHttpClient(), new Gson());
+    ProgressBar vLoadingActions;
+    TextView vQuantityActions;
+    CompositeDisposable disposables = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actions);
+
+        vLoadingActions = findViewById(R.id.loadingActionsBar);
+        vQuantityActions = findViewById(R.id.quantityActions);
+
+        disposables.add(
+            actionsInfo.updateQuantity().subscribeWith(new DisposableObserver<Integer>() {
+                @Override
+                public void onNext(@NonNull Integer integer) {
+
+                }
+
+                @Override
+                public void onError(@NonNull Throwable e) {
+
+                }
+
+                @Override
+                public void onComplete() {
+                    Log.v("tag",  actionsInfo.quantity.toString());
+                    vLoadingActions.setVisibility(View.GONE);
+                    vQuantityActions.append(actionsInfo.quantity.toString());
+
+                    actionsInfo.liveQuantity()
+                    .observeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribeWith(new DisposableObserver<Integer>() {
+                        @SuppressLint("SetTextI18n")
+                        @Override
+                        public void onNext(@NonNull Integer integer) {
+                            Log.v("tag", actionsInfo.quantity.toString());
+                            vQuantityActions.setText("Actions " + actionsInfo.quantity.toString());
+                        }
+
+                        @Override
+                        public void onError(@NonNull Throwable e) {
+
+                        }
+
+                        @Override
+                        public void onComplete() {
+
+                        }
+                    });
+                }
+            })
+        );
     }
 }
--- app/src/main/java/com/example/hackernews/ActionsApi.java
+++ app/src/main/java/com/example/hackernews/ActionsApi.java
@@ -1,13 +1,20 @@
 package com.example.hackernews;
 
 import android.icu.text.CaseMap;
+import android.util.TimeUtils;
 
 import com.google.gson.Gson;
 
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Emitter;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.ObservableEmitter;
 import io.reactivex.rxjava3.core.ObservableOnSubscribe;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.observers.DisposableObserver;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import okhttp3.Call;
 import okhttp3.OkHttpClient;
@@ -28,12 +35,48 @@ public class ActionsApi {
         this.gson = gson;
     }
 
-    Observable<Integer> getQuantityReq() {
+    public Observable<Integer> liveQuantity() {
+        return Observable.create(e -> {
+            updateQuantityWidthDaley(e, new Disposable[] {null});
+        });
+    }
+    private void updateQuantityWidthDaley(Emitter<Integer> e, Disposable[] disposable) {
+        disposable[0]
+        = updateQuantity()
+            .delay(1, TimeUnit.SECONDS)
+            .subscribeWith(new DisposableObserver<Integer>() {
+                @Override
+                public void onNext(@NonNull Integer integer) {
+                    e.onNext(integer);
+                    updateQuantityWidthDaley(e, disposable);
+                }
+
+                @Override
+                public void onError(@NonNull Throwable e) {
+
+                }
+
+                @Override
+                public void onComplete() {
+
+                }
+            });
+    }
+
+    public Observable<Integer> updateQuantity() {
+        return getQuantityReq().observeOn(AndroidSchedulers.mainThread()).map(e -> {
+            this.quantity = e;
+            return e;
+        });
+    }
+
+    private Observable<Integer> getQuantityReq() {
         return Observable.create((ObservableOnSubscribe<Response>) emitter -> {
             Call req = httpClient.newCall(instanceQuantityRequest);
             emitter.setCancellable(req::cancel);
 
             emitter.onNext(req.execute());
+            emitter.onComplete();
         })
         .subscribeOn(Schedulers.io())
         .map(e -> e.body().string())
