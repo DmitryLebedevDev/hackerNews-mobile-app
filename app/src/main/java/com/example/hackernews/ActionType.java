c4ada50e9d30ac139fc742bc719dd90f33106204
Gercog52 <54477063+Gercog52@users.noreply.github.com>
2021-02-05 20:50:13 +0800

    add convert time action to ago string

--- app/src/main/java/com/example/hackernews/ActionType.java
+++ app/src/main/java/com/example/hackernews/ActionType.java
@@ -1,10 +1,34 @@
 package com.example.hackernews;
 
+import android.text.format.Time;
+
 public abstract class ActionType {
     public Integer id;
     public Integer time;
     public String by;
     public Boolean deleted;
+
+    public String getTimeAgo(Integer currentDate) {
+        Integer different = currentDate - time;
+
+        if(different >= Time.YEAR) {
+            Integer years = different / Time.HOUR;
+            return years >= 1 ?
+                years + " years ago"
+                :
+                "1 year ago";
+        } else if (different >= Time.MONTH) {
+            Integer months = different / Time.HOUR;
+            return months >= 1 ?
+                    months + " months ago"
+                    :
+                    "1 month ago";
+        } else if (true) {
+
+        }
+
+        return null;
+    }
 }
 
 
